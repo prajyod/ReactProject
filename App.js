b334e62a058aad913d88ce08d1e5d974950784c4
prajyod arudra <prajyod@prajyods-MacBook-Pro.local>
2023-05-23 16:33:37 -0500

    intial code for react

---
+++ App.js
@@ -0,0 +1,29 @@
+
+/**
+ * <div id="parent">
+    <div id="child">
+        <h1 id="header">
+            Hello from H1 tag.
+        </h1>
+        <h2 id="header2">
+            Hello from H2 tag.
+        </h2>
+    </div>
+    <div id="child2">
+        <h1 id="header">
+            Hello from H1 tag.
+        </h1>
+        <h2 id="header2">
+            Hello from H2 tag.
+        </h2>
+    </div>
+    </div>
+ */
+
+const react = React.createElement("div", { id: "parent" },
+ [React.createElement("div", { id: "child" }, 
+ [React.createElement("h1", {id:"header"}, "Hello from H1 tag"),React.createElement("h2", {id:"header"}, "Hello from H2 tag")]),React.createElement("div", { id: "child2" }, 
+ [React.createElement("h1", {id:"header"}, "Hello from H1tag"),React.createElement("h2", {id:"header"}, "Hello from H2 tag")])]);
+
+const reactDOM = ReactDOM.createRoot(document.getElementById("root"));
+reactDOM.render(react);
